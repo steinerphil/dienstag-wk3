8de8396fc1480cc5829630d7c179263dbd91de97
Philipp Steiner <philipp-steiner@gmx.de>
2021-09-21 16:15:21 +0200

    git init

---
+++ src/main/java/com/example/dienstagwk3/DienstagWk3Application.java
@@ -0,0 +1,13 @@
+package com.example.dienstagwk3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DienstagWk3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DienstagWk3Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/dienstagwk3/DienstagWk3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dienstagwk3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DienstagWk3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
